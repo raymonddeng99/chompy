3efb377d09d44bf33071e1265bd47ea83f42e72f
Raymond Deng <1107450@gmail.com>
2023-09-07 20:21:19 -0700

    add test

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+open Chompy.Http_protocol
+
+let main () = 
+	let example_url = "https://en.wikipedia.org/wiki/Monty_Python_and_the_Holy_Grail" in
+	let html_response = Http_protocol.fetch example_url |> Lwt_main.run in
+	(* let document = Http_protocol.parse_html_response html_response in *)
+	print_endline html_response
+
+let () = main ()
